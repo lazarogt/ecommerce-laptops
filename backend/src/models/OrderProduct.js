e553b7da1d7520ebddd1a6501b6da5bcadaeb01f
lazarogt <lazarogonzaleztorres091@gmail.com>
2026-02-10 17:04:11 -0500

    Sobrescribiendo remoto con la version local completa

---
+++ backend/src/models/OrderProduct.js
@@ -0,0 +1,21 @@
+// backend/src/models/OrderProduct.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const OrderProduct = sequelize.define('OrderProduct', {
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 1,
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    defaultValue: 0,
+  },
+}, {
+  tableName: 'OrderProducts',
+  timestamps: false,
+});
+
+module.exports = OrderProduct;
